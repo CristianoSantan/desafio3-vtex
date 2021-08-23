babbdf88135d64597185aaf4e0aa1bdf794aa4a4
CristianoSantan <cristiano.mer@gmail.com>
2021-08-23 00:24:36 -0300

    readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hiring Coders Challenge | VTex & Gama Academy
+
+Desafio#3 - VTEX IO + AWS
+
+Você foi contratado para desenvolver o ecommerce do
+seu novo cliente, a UPMedal.com.
+
+Seguindo o briefing para desenvolvimento:
+Template de criação
+
+- Criar a solução a partir de um boiler template padrão da Vtex IO.
+
+Obs.: Gostaria de ter mais tempo para completar o que faltou, como estilização, retornar os preços no card o product.summury e utilizar a api da AWS.
--- api/react/LeadForm.tsx
+++ api/react/LeadForm.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import './leadForm.css';
 
 const LeadForm: StorefrontFunctionComponent<LeadFormProps> = () => {
+
     return (
-        <div>
-            <form>
-                <label htmlFor="name">Nome</label>
+        <div className="form">
+            <form style={{
+                color: 'white',
+                display: 'flex',
+                flexDirection: 'column',
+                margin: '200px auto',
+                width: '500px'
+            }}>
+                <label htmlFor="name" style={{ marginTop: '10px' }}>Nome</label>
                 <input type="text" name="name" />
-                <label htmlFor="email">Email</label>
+                <label htmlFor="email" style={{ marginTop: '10px' }}>Email</label>
                 <input type="email" name="email" />
-                <label htmlFor="phone">Telefone</label>
+                <label htmlFor="phone" style={{ marginTop: '10px' }}>Telefone</label>
                 <input type="number" name="phone" />
-                <input type="submit" value="Enviar" />
+                <input
+                    type="submit"
+                    value="Enviar"
+                    style={{
+                        marginTop: '10px'
+                    }} />
             </form>
         </div>
     )
